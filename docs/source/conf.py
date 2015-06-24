4d54ee21ef7de86c3e4f605286720effb07a7fe6
Vladimir Pozniak <povlig@gmail.com>
2015-06-24 08:00:16 +0300

    Исправления
    пропал pdf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -213,11 +213,11 @@ latex_elements = {
 #'pointsize': '10pt',
 
 # Additional stuff for the LaTeX preamble.
-'preamble': '\\usepackage[utf8]{inputenc}',
-'babel': '\\usepackage[russian]{babel}',
-'cmappkg': '\\usepackage{cmap}',
-'fontenc': '\usepackage[T1,T2A]{fontenc}',
-'utf8extra':'\\DeclareUnicodeCharacter{00A0}{\\nobreakspace}',
+#'preamble': '\\usepackage[utf8]{inputenc}',
+#'babel': '\\usepackage[russian]{babel}',
+#'cmappkg': '\\usepackage{cmap}',
+#'fontenc': '\usepackage[T1,T2A]{fontenc}',
+#'utf8extra':'\\DeclareUnicodeCharacter{00A0}{\\nobreakspace}',
 
 # Latex figure (float) alignment
 #'figure_align': 'htbp',
